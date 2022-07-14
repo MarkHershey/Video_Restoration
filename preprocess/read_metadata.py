bb222ad2a2206d26a7101d982570d7071554549e
Mark Huang <hhzeos@gmail.com>
2022-07-15 01:22:48 +0800

    Update fourcc reading

--- preprocess/read_metadata.py
+++ preprocess/read_metadata.py
@@ -7,6 +7,30 @@ from pathlib import Path
 import cv2
 
 
+def get_fourcc(fourcc_str: str):
+    fourcc_int = cv2.VideoWriter_fourcc(*fourcc_str)
+    return fourcc_int
+
+
+def hex_to_decimal(hex_str: str):
+    return int(hex_str, 16)
+
+
+def get_fourcc_str(cap: cv2.VideoCapture) -> str:
+    # get fourcc from opencv
+    fourcc_decimal: int = int(cap.get(cv2.CAP_PROP_FOURCC))
+    # convert decimal to hex
+    fourcc_hex: str = hex(fourcc_decimal)
+    assert len(fourcc_hex) == 10, "Invalid fourcc"
+    # remove 0x from hex
+    fourcc_hex = fourcc_hex[2:]
+    # convert hex to ascii
+    fourcc_str: str = ""
+    for i in range(0, len(fourcc_hex), 2):
+        fourcc_str += chr(hex_to_decimal(fourcc_hex[i : i + 2]))
+    return fourcc_str[::-1].upper()
+
+
 def read_metadata(video_filepath: str):
     try:
         # get OpenCV version
@@ -14,14 +38,16 @@ def read_metadata(video_filepath: str):
         print(f"OpenCV version: {major_ver}.{minor_ver}.{subminor_ver}")
 
         # create a VideoCapture object and read from input file
-        cap = cv2.VideoCapture(video_filepath)
+        cap: cv2.VideoCapture = cv2.VideoCapture(video_filepath)
 
         # get video metadata
+        # ref: https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
         metadata = dict(
-            width=cap.get(cv2.CAP_PROP_FRAME_WIDTH),
-            height=cap.get(cv2.CAP_PROP_FRAME_HEIGHT),
+            width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
             fps=cap.get(cv2.CAP_PROP_FPS),
-            fourcc=cap.get(cv2.CAP_PROP_FOURCC),
+            # fourcc=int(cap.get(cv2.CAP_PROP_FOURCC)),
+            fourcc=get_fourcc_str(cap),
             frame_count=cap.get(cv2.CAP_PROP_FRAME_COUNT),
             brightness=cap.get(cv2.CAP_PROP_BRIGHTNESS),
             contrast=cap.get(cv2.CAP_PROP_CONTRAST),
